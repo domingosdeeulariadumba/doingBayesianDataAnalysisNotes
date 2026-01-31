7bc639c41473407faeb5785c37b63a51de6e1108
Domingos de Eularia Dumba <domingosdeeulariadumba@users.noreply.github.com>
2026-01-31 19:48:03 +0100

    Add Long-run relative frequency for a six-sided die

---
+++ ch2/RunningProportionDieVersion.py
@@ -0,0 +1,30 @@
+# Dependencies
+import numpy as np
+from matplotlib import pyplot as plt
+
+# A function for computing the long-run relative frequency
+def longrun_relative_frequency(trials: int) -> np.ndarray: 
+    '''It returns the long-run relative frequency of coming up a six on a six-sided die'''
+    # Pseudo-Random Generated Number
+    # np.random.seed(47405) I'm supressing the seed here
+    prng = np.random.choice([i for i in range(1, 7)], trials)
+    is_six = prng == 6
+    
+    # Computing relative frequency
+    cumsum = np.cumsum(is_six)
+    counts = np.arange(1, prng.size + 1)
+    result = np.array([i / j for i, j in zip(cumsum, counts)])
+    return result
+
+
+# Plotting the results
+fig, axes = plt.subplots(2, 2, sharey = True)
+t = 'Long-run relative frequency of having a 6 on a six-sided die — p(x = 6)'
+p = round(1/6, 3)
+for trials, ax in zip(np.geomspace(1e3, 1e6, 4), axes.flatten()):
+    data = longrun_relative_frequency(int(trials))
+    ax.plot(data, 'c', label = f'N = {data.size}', linewidth = .9)
+    ax.axhline(p, color = 'k', linestyle = ':', label = f'P(x) = {p}', linewidth = .9)
+    ax.legend()
+plt.suptitle(t, fontsize = 10)
+plt.show()
