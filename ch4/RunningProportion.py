241a25804c9f570655e9ee9f158e79751b4eacbb
Domingos de Eularia Dumba <domingosdeeulariadumba@users.noreply.github.com>
2026-01-31 21:34:43 +0100

    Add RunningProportion script and derived plot

---
+++ ch4/RunningProportion.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def longrun_relative_frequency(trials: int = 500) -> np.ndarray: 
+    np.random.seed(47405)
+    prng = np.random.choice(['H', 'T'], trials)
+    is_head = prng == 'H'    
+    cumsum = np.cumsum(is_head)
+    counts = np.arange(1, prng.size + 1)
+    run_prop = np.array([i / j for i, j in zip(cumsum, counts)])
+    display_string = ''.join(prng[:11]) + '...' + f'\nEnd Proportion = {round(run_prop[-1], 2)}'
+    return run_prop, display_string
+
+
+run_prop, display_string = longrun_relative_frequency()
+plt.plot(run_prop,'-o', color = 'skyblue', label = display_string)
+plt.axhline(.5, color = 'k', linestyle = ':')
+plt.xlabel('Flip Numbers', labelpad = 1.5)
+plt.ylabel('Proportion Heads', labelpad = 1.5)
+plt.legend(frameon = False, handlelength = 0, markerscale = 0)
+plt.title('Running Proportion of Heads')
+plt.show()
